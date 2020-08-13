7208b5623083d81598659fe8b7bccfa4a8000205
Raghav Agarwal <agarwal.raghav0704@gmail.com>
2020-08-13 19:59:56 +0530

    Update 371.Sum of Two Integers.cpp

--- Bit Manipulation/371.Sum of Two Integers.cpp
+++ Bit Manipulation/371.Sum of Two Integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        while(b!=0){
+            int t=a;
+            a=a^b;
+            b=(b & t & INT_MAX)<<1;//INT_MAX is done because leetcode does not allow left shift if first bit is 1 in which case it overflows.
+        }
+        return a;
     }
 };
